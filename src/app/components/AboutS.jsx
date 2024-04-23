136f74b6dc2285f5a6cb134aa05a20c3b24a6cd2
quiles <quilesxasterin8@gmail.com>
2024-04-23 14:17:05 -0500

    first commit

--- src/app/components/AboutS.jsx
+++ src/app/components/AboutS.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useTransition, useState } from 'react'
+import React, { useState, useTransition } from 'react'
 import Image from 'next/image'
 import Tabbtn from './Tabbtn';
 
